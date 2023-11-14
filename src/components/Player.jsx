25b794573879878d51a9ac48e0e56519ac41908b
Yash1Hingu <yash23hingu@gmail.com>
2023-11-14 17:49:07 +0530

    Added State in edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 export default function Player({ name , symbol }) {
+    const [isEditing,setEditing] = useState(false);
+
+    function handlerEditClick() {
+        setEditing(isEditing ? false : true);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    if(isEditing){
+        playerName = <input type="text" required/>;
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handlerEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
